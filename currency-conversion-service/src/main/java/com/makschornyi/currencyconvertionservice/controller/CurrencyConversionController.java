d38202436edf2631d7f1f11750371f629036d962
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2020-11-22 01:37:00 +0200

    Added Feign and Ribbon

--- currency-conversion-service/src/main/java/com/makschornyi/currencyconvertionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/makschornyi/currencyconvertionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.makschornyi.currencyconvertionservice.controller;
 
 import com.makschornyi.currencyconvertionservice.model.CurrencyConversionBean;
+import com.makschornyi.currencyconvertionservice.proxy.CurrencyExchangeServiceProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,27 @@ import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    private final CurrencyExchangeServiceProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    @GetMapping("/currency-converter-proxy/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertProxy(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversionBean(
+                response.getId(),
+                from,
+                to,
+                response.getConversionMultiple(),
+                quantity,
+                quantity.multiply(response.getConversionMultiple()),
+                response.getPort());
+    }
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convert(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
