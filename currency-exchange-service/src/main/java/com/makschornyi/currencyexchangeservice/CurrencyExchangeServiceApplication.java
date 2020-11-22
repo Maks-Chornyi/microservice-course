b206ec18a6077db5ccda33a2d521ea6196461987
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2020-11-23 00:08:19 +0200

    Added exchange service to register in discovery

--- currency-exchange-service/src/main/java/com/makschornyi/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/makschornyi/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,9 +2,10 @@ package com.makschornyi.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
