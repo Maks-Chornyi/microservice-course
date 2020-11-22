1e55dfda7b3dafa5b6cc36181438c46047a594f4
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2020-11-22 23:54:16 +0200

    Added convertion service to register in eureka

--- currency-conversion-service/src/main/java/com/makschornyi/currencyconvertionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/makschornyi/currencyconvertionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.makschornyi.currencyconvertionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.makschornyi.currencyconvertionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
