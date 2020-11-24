cd846a40a2a1b9fc7300244f09b1237f4f81b611
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2020-11-24 23:25:12 +0200

    added zuul api gateway to the architecture

---
+++ netflix-zuul-api-gateway-service/src/main/java/com/makschornyi/netflixzuulapigatewayservice/NetflixZuulApiGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.makschornyi.netflixzuulapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class NetflixZuulApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServiceApplication.class, args);
+	}
+
+}
